9a16e4bc8871a0d5f311463ca9d48b1fe2928f65
Ryan Faircloth <ryan@dss-i.com>
2023-08-16 18:13:48 -0400

    feat: initial

---
+++ main.tf
@@ -0,0 +1,60 @@
+#-------------------------------
+# Local Declarations
+#-------------------------------
+locals {
+  account_tier             = (var.account_kind == "FileStorage" ? "Premium" : split("_", var.skuname)[0])
+  account_replication_type = (local.account_tier == "Premium" ? "LRS" : split("_", var.skuname)[1])
+}
+
+
+
+resource "azurerm_storage_account" "storeacc" {
+  name                      = var.name
+  resource_group_name       = var.resource_group_name
+  location                  = var.location
+  account_kind              = var.account_kind
+  account_tier              = local.account_tier
+  account_replication_type  = local.account_replication_type
+  enable_https_traffic_only = true
+  min_tls_version           = var.min_tls_version
+  tags                      = var.tags
+
+  dynamic "identity" {
+    for_each = var.managed_identity_type != null ? [1] : []
+    content {
+      type         = var.managed_identity_type
+      identity_ids = var.managed_identity_type == "UserAssigned" || var.managed_identity_type == "SystemAssigned, UserAssigned" ? var.managed_identity_ids : null
+    }
+  }
+
+  blob_properties {
+    delete_retention_policy {
+      days = var.blob_soft_delete_retention_days
+    }
+    container_delete_retention_policy {
+      days = var.container_soft_delete_retention_days
+    }
+    versioning_enabled       = var.enable_versioning
+    last_access_time_enabled = var.last_access_time_enabled
+    change_feed_enabled      = var.change_feed_enabled
+  }
+
+  dynamic "network_rules" {
+    for_each = var.network_rules != null ? ["true"] : []
+    content {
+      default_action             = "Deny"
+      bypass                     = var.network_rules.bypass
+      ip_rules                   = var.network_rules.ip_rules
+      virtual_network_subnet_ids = var.network_rules.subnet_ids
+    }
+  }
+}
+
+#--------------------------------------
+# Storage Advanced Threat Protection 
+#--------------------------------------
+resource "azurerm_advanced_threat_protection" "atp" {
+  target_resource_id = azurerm_storage_account.storeacc.id
+  enabled            = var.enable_advanced_threat_protection
+}
+
