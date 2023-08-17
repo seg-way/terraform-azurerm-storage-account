033e8fbdc87d3c1b1d48ca093b5748c2c7a7378d
Ryan Faircloth <ryan@dss-i.com>
2023-08-16 20:34:44 -0400

    feat: output id

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output "storage_account_id" {
   description = "The ID of the storage account."
   value       = azurerm_storage_account.storeacc.id
 }
+output "id" {
+  description = "The ID of the storage account."
+  value       = azurerm_storage_account.storeacc.id
+}
 
 output "storage_account_name" {
   description = "The name of the storage account."
